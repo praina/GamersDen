960d7e7e559a0e6de28a44684e440e79808bcf77
praina <prateekraina2012@gmail.com>
2016-12-20 02:18:57 +0530

    Maps enhanced : Edit 1
    User can now add a marker on the maps activity and see the displacement
    between the two markers.
    Yet to be created : Route instead of displacement

--- app/src/main/java/com/example/android/gamersden/MapsActivity.java
+++ app/src/main/java/com/example/android/gamersden/MapsActivity.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,7 +21,20 @@ import java.util.List;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final String TAG = "MapsActivity";
+
     private GoogleMap mMap;
+    private EditText findRouteEdittext;
+    private Button findRouteButton;
+
+    private String searchString;
+
+    private Geocoder coder, coder2;
+    private List<Address> addressList, addressList2;
+    private LatLng coord,coord2;
+    private MarkerOptions mMarkerOptions,mMarkerOptions2,mMarkerOptions3;
+    private TextInputLayout findRouteEditTextInputLayout;
+    private String completeAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +44,96 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        findRouteEdittext = (EditText)findViewById(R.id.findRouteEdittext);
+        findRouteButton = (Button)findViewById(R.id.findRouteButton);
+        findRouteEditTextInputLayout = (TextInputLayout) findViewById(R.id.findRouteEdittextInputLayout);
+
+//        findRouteEdittext.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                if (!findRouteEdittext.getText().toString().trim().equals(""))
+//                {
+//                    if (findRouteEditTextInputLayout != null) {
+//                        findRouteEditTextInputLayout.setError(null);
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//
+//            }
+//        });
+
+
+        findRouteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                searchString = findRouteEdittext.getText().toString().toLowerCase().trim();
+
+
+                if (searchString.equals("")){
+//                    findRouteEditTextInputLayout.setErrorEnabled(true);
+//                    findRouteEditTextInputLayout.setError("Enter a location or City!");
+                    //use snackbar to show something.
+
+                }
+                else{
+
+                    coder2 = new Geocoder(MapsActivity.this);
+                    coord2 = null;
+
+                    try {
+                        addressList2 = coder2.getFromLocationName(searchString, 5);
+                        if (addressList2 != null) {
+
+                            Address location = addressList2.get(0);
+
+                            coord2 = new LatLng(location.getLatitude(), location.getLongitude());
+                        }
+
+                    } catch (Exception ex) {
+
+                        ex.printStackTrace();
+                    }
+
+//                    Log.e(TAG, "This is coord2 : " + coord2 + " " + searchString);
+//
+//                    Toast.makeText(MapsActivity.this, "This is coord2 + ET VALUE : " + coord2 + " " + searchString,Toast.LENGTH_LONG).show();
+
+                    if (coord2!=null && coord!=null) {
+                        mMap.clear();
+                        mMarkerOptions2 = new MarkerOptions();
+                        mMarkerOptions2.position(coord2);
+                        mMarkerOptions2.title(searchString); //this is the address of the new string entered by the user
+                        mMap.addMarker(mMarkerOptions2).showInfoWindow();
+
+                        mMarkerOptions3 = new MarkerOptions();
+                        mMarkerOptions3.position(coord);
+                        mMarkerOptions3.title(completeAddress); //this is the address of the original string received from intent
+                        mMap.addMarker(mMarkerOptions3).showInfoWindow();
+
+//                        PolylineOptions polylineOptions = new PolylineOptions().add(coord,coord2).width(5)
+//                                .color(ContextCompat.getColor(MapsActivity.this,R.color.colorPrimary));
+//                        mMap.addPolyline(polylineOptions);
+                        //Instead of polyline we need route!
+
+                    }
+                    else {
+                        //Snackbar : cannot find the given location
+                    }
+
+                }
+            }
+        });
+
     }
 
 
@@ -43,17 +150,16 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
 
         Intent intent = getIntent();
-        Geocoder coder = new Geocoder(this);
-        List<Address> address;
-        LatLng coord = null;
+        coder = new Geocoder(this);
+        coord = null;
 
-        String completeAddress = intent.getStringExtra("completeAddress");
+        completeAddress = intent.getStringExtra("completeAddress");
 
         try {
-            address = coder.getFromLocationName(completeAddress, 5);
-            if (address != null) {
+            addressList = coder.getFromLocationName(completeAddress, 5);
+            if (addressList != null) {
 
-                Address location = address.get(0);
+                Address location = addressList.get(0);
 
                 coord = new LatLng(location.getLatitude(), location.getLongitude());
             }
@@ -65,12 +171,18 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         mMap = googleMap;
 
-        MarkerOptions mMarkerOptions = new MarkerOptions();
-        mMarkerOptions.position(coord);
-        mMap.addMarker(mMarkerOptions.title(completeAddress));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(coord));
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coord, 14.0f));
+        if (coord!=null) {
 
+            mMarkerOptions = new MarkerOptions();
+            mMarkerOptions.position(coord);
+            mMarkerOptions.title(completeAddress);
+            mMap.addMarker(mMarkerOptions).showInfoWindow();
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(coord));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coord, 14.0f));
+        }
+        else{
+            //Snackbar : cannot find the given location
+        }
         //Toast.makeText(this, completeAddress + coord,Toast.LENGTH_LONG).show();
     }
 
